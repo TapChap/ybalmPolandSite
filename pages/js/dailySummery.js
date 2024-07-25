ced044a7cf8cfd4f70f98e6cc05a4a1b3782d243
TapChap <shaigimel@gmail.com>
2024-07-25 22:37:53 +0300

    extract js to separate files

---
+++ pages/js/dailySummery.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getStorage, ref, listAll, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtFxfJfWYzTkjCMrhySoPofAS5zAbmqKM",
+    authDomain: "ybalmpolandsite.firebaseapp.com",
+    projectId: "ybalmpolandsite",
+    storageBucket: "ybalmpolandsite.appspot.com",
+    messagingSenderId: "581656278199",
+    appId: "1:581656278199:web:d43566dcc3a87093c50932",
+    measurementId: "G-Z8HQTJMV7V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+const summeries = document.getElementById('summeries');
+
+async function loadSummeries() {
+    const folder = await listAll(ref(storage, 'dailySummary'));
+    console.log(folder.items.length);
+
+    for (let i = folder.items.length - 1; i >= 0; i --){
+        try {
+            const url = await getDownloadURL(folder.items[i]);
+            const response = await fetch(url);
+            const content = await response.text();
+
+            const daySummery = document.createElement("div");
+            daySummery.className = "day-summary";
+
+            const title = document.createElement("h2");
+            const paragraph = document.createElement("p");
+
+            title.textContent = `${i + 1} יום`;
+            paragraph.textContent = content;
+
+            daySummery.append(title);
+            daySummery.append(paragraph);
+
+            summeries.append(daySummery);
+
+        } catch (error) {
+            console.log('Error listing images:', error);
+        }
+    }
+}
+
+loadSummeries();
---
+++ pages/js/gallery.js
@@ -0,0 +1,51 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+import { getStorage, ref, listAll, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBtFxfJfWYzTkjCMrhySoPofAS5zAbmqKM",
+    authDomain: "ybalmpolandsite.firebaseapp.com",
+    projectId: "ybalmpolandsite",
+    storageBucket: "ybalmpolandsite.appspot.com",
+    messagingSenderId: "581656278199",
+    appId: "1:581656278199:web:d43566dcc3a87093c50932",
+    measurementId: "G-Z8HQTJMV7V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
+
+const gallery = document.getElementById('gallery');
+
+async function loadImages() {
+    for (let i = 10; i >= 0; i--) {
+        const storageRef = ref(storage, `day${i}`);
+        const listResult = await listAll(storageRef);
+        const downloadPromises = listResult.items.map(itemRef => getDownloadURL(itemRef));
+        const urls = await Promise.all(downloadPromises);
+
+        if (listResult.items.length > 0) {
+
+            const hr = document.createElement("hr");
+            const dayNum = document.createElement("h2");
+            dayNum.textContent = `${i} יום`;
+            gallery.appendChild(hr);
+            gallery.appendChild(dayNum);
+
+            const dayBox = document.createElement("div");
+            dayBox.className = 'day-box';
+
+            for (const url of urls) {
+                const img = document.createElement('img');
+                img.src = url;
+                dayBox.appendChild(img);
+            }
+
+            gallery.appendChild(dayBox);
+        }
+    }
+}
+
+await loadImages();
+await document.getElementById("loading").remove();
+document.getElementById('gallery').style.visibility = 'visible';
